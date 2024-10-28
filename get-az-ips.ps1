0b331b82a5f8d4a1620fca2eee13c105d4e1f06b
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2024-10-28 16:46:14 -0400

    Initial commit

---
+++ get-az-ips.ps1
@@ -0,0 +1,20 @@
+<#
+Microsoft does not provide a list to json file with the Azure IP ranges. 
+The only way to get the list is to parse the HTML of a page that contains the link to the json file.
+
+This script will parse that page for the json file and then post it to github
+
+#>
+
+## Vars
+$AzurePublicIpRanges = "https://www.microsoft.com/en-us/download/confirmation.aspx?id=56519"
+## Get the response
+$Response = Invoke-WebRequest -Uri $AzurePublicIpRanges
+## Get all of the json links in the href tags and select the unique one since for some reason the page publishes several
+$json_url = $Response.links.href | Sort-Object | Where-Object {$_ -match "json"} | Select-Object -Unique
+## Now that we have the link, extract the file
+Invoke-WebRequest -Uri $json_url -OutFile "az_ips.json"
+
+
+## Compare to gcp
+$r2 = Invoke-RestMethod -Uri "https://www.gstatic.com/ipranges/cloud.json"
